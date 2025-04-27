43bb5ec678c1684c46e3d8651be5f83f564f58ae
mohamed <mohamedismaelq145@gmail.com>
2025-04-27 23:37:00 +0200

    Completed sebha_tab

--- lib/home/tabs/radio/radio_tab.dart
+++ lib/home/tabs/radio/radio_tab.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:islami_online_c13/utils/app_colors.dart';
+
+import '../../../utils/my_theme_data.dart';
 
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-    );
+    return Stack(children: [
+      Image.asset(
+        "assets/images/",
+        width: double.infinity,
+        fit: BoxFit.fitWidth,
+      ),
+      Scaffold(
+        body: Column(
+          children: [
+            Expanded(child: Image.asset("assets/images/radioIcon.png")),
+            Text("إذاعة القرآن الكريم ",style:Theme.of(context).textTheme.bodyLarge,),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:  [
+                  Icon(Icons.skip_previous,color: AppColors.primaryDark,size:60,),
+                  Icon(Icons.play_arrow,color: AppColors.primaryDark,size:60,),
+                  Icon(Icons.skip_next,color: AppColors.primaryDark,size:60,)
+                ],
+              ),
+            )
+
+          ],
+        ),
+      )
+    ]);
   }
 }
+
+
--- lib/home/tabs/sebha/sebha_tab.dart
+++ lib/home/tabs/sebha/sebha_tab.dart
@@ -1,9 +1,116 @@
-import 'package:flutter/material.dart';
 
-class SebhaTab extends StatelessWidget {
+
+import 'package:flutter/material.dart';
+import 'package:islami_online_c13/utils/app_styles.dart';
+
+class SebhaTab extends StatefulWidget {
+  const SebhaTab({super.key});
+
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  double turns = 0;
+  int num = 0;
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    Size size = MediaQuery.sizeOf(context);
+    return Expanded(
+      child: Column(
+        children: [
+          Image.asset('assets/images/logo.png'),
+          SizedBox(
+            height: size.height * 0.016,
+          ),
+          Text(
+            'سَبِّحِ اسْمَ رَبِّكَ الأعلى',
+            style: AppStyles.bold16White.copyWith(
+              fontSize: 36,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.1,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: size.width * 0.12,
+              ),
+              Image.asset('assets/images/sebhaHeader.png'),
+            ],
+          ),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              AnimatedRotation(
+                turns: turns,
+                duration: const Duration(seconds: 1),
+                child: Image.asset(
+                  'assets/images/sebhaBody.png',
+                ),
+              ),
+              Positioned(
+                child: InkWell(
+                  customBorder: const CircleBorder(),
+                  overlayColor:
+                  const WidgetStatePropertyAll(Colors.transparent),
+                  onTap: () {
+                    setState(() => turns += 1 / 30);
+                    num++;
+                    if (num > 100) {
+                      num = 0;
+                    }
+                    changer();
+                  },
+                  child: Container(
+                    decoration: const ShapeDecoration(shape: CircleBorder()),
+                    padding: const EdgeInsets.all(80),
+                    child: Column(
+                      children: [
+                        Text(
+                          changer(),
+                          style: num == 100
+                              ? AppStyles.White14
+                              : AppStyles.bold20White.copyWith(
+                            fontSize: 36,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Text(
+                          '$counter',
+                          style: AppStyles.bold20White.copyWith(
+                            fontSize: 36,)
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
+  }
+
+  String changer() {
+    if (num > 33 && num <= 66) {
+      counter = num - 33;
+      return 'الحمد الله';
+    } else if (num > 66 && num <= 99) {
+      counter = num - 66;
+      return 'الله أكبر';
+    } else if (num == 100) {
+      counter = 0;
+      return 'لا إله إلا الله وحده لا شريك له له الملك\n وله الحمد وهو على كل شيء قدير';
+    }
+    counter = num;
+    return 'سبحان الله';
   }
 }
